6eff61b71bc5b42cafa3468a25a324c6db49e509
Tufahel Ahmed <tufahel96@gmail.com>
2024-07-07 20:41:56 +0600

    React server component

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,7 +1,10 @@
+import Counter from "@/components/Counter/Counter";
+
 const HomePage = () => {
   return (
       <div>
           <h1>Welcome to next js home Page</h1>
+          <Counter />  
       </div>
   );
 };
---
+++ my-app/src/components/Counter/Counter.js
@@ -0,0 +1,16 @@
+"use client";
+import React, { useState } from "react";
+
+const Counter = () => {
+    const [counter, setCounter] = useState(0);
+    return (
+        <div>
+            <h1>This is counter page</h1>
+            <h1>Counter {counter}</h1>
+            <button className="border p-2" onClick={() => setCounter(counter + 1)}>Increase</button>
+          <button className="border p-2" onClick={() => setCounter(counter - 1)}>Decrease</button>
+        </div>
+    );
+};
+
+export default Counter;
