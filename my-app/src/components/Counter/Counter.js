00122e7899c7a051d62a7d0fc0c9e03bdb1752d8
Tufahel Ahmed <tufahel96@gmail.com>
2024-07-08 15:20:24 +0600

    group routing with different layout

--- my-app/src/components/Counter/Counter.js
+++ my-app/src/components/Counter/Counter.js
@@ -7,8 +7,8 @@ const Counter = () => {
         <div>
             <h1>This is counter page</h1>
             <h1>Counter {counter}</h1>
-            <button className="border p-2" onClick={() => setCounter(counter + 1)}>Increase</button>
-          <button className="border p-2" onClick={() => setCounter(counter - 1)}>Decrease</button>
+            <button className="btn btn-accent" onClick={() => setCounter(counter + 1)}>Increase</button>
+          <button className="btn btn-accent" onClick={() => setCounter(counter - 1)}>Decrease</button>
         </div>
     );
 };
